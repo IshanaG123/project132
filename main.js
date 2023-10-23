07e6e395affb8ed2b210b853459b35f34cb2f939
IshanaG123 <146230555+IshanaG123@users.noreply.github.com>
2023-10-23 16:37:10 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function Bed_room(){
+    window.location ="bd.html";
+}
+
+function TV_and_AC(){
+    window.location ="tv.html";
+}
+
+function desk(){
+    window.location ="desk.html";
+}
+
+function bottles(){
+    window.location ="bot.html";
+}
+
+function fruit_basket(){
+    window.location ="fruits.html";
+}
